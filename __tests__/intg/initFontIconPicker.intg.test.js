9a1c0f9daf972eb87cc3694ccd6fd3bc580d1344
Swashata Ghosh <swashata@wpquark.com>
2018-03-24 14:28:47 +0530

    test: add more test for initFontIconPicker
    Add tests for invocation of APIs.

--- __tests__/intg/initFontIconPicker.intg.test.js
+++ __tests__/intg/initFontIconPicker.intg.test.js
@@ -1,9 +1,14 @@
 import jQuery from 'jquery';
 import initFontIconPicker from '../../src/js/modules/initFontIconPicker.js';
-let fip;
+let fip, testSelect, testFip;
 
 beforeAll( () => {
 	fip = initFontIconPicker( jQuery );
+} );
+
+beforeEach( () => {
+	testSelect = jQuery( '<select><option value="">none</option><option value="fa fa-arrow"></option>' );
+	testFip = testSelect.fontIconPicker();
 } );
 
 test( 'initialize fontIconPicker', () => {
@@ -22,12 +27,41 @@ test( 'attach fontIconPicker to jQuery.fn', () => {
 } );
 
 test( 'fontIconPicker has APIs', () => {
-	const testSelect = jQuery( '<select><option value="">none</option><option value="fa fa-arrow"></option>' );
-	const testFip = testSelect.fontIconPicker();
-
 	expect( typeof testFip.setIcon ).toBe( 'function' );
 	expect( typeof testFip.setIcons ).toBe( 'function' );
 	expect( typeof testFip.destroyPicker ).toBe( 'function' );
 	expect( typeof testFip.refreshPicker ).toBe( 'function' );
 	expect( typeof testFip.repositionPicker ).toBe( 'function' );
+} );
+
+test( 'API setIcons work', () => {
+	const setIcons = testSelect.data( 'fontIconPicker' ).setIcons = jest.fn();
+	testFip.setIcons( {}, false );
+	expect( setIcons ).toHaveBeenCalledTimes( 1 );
+} );
+
+test( 'API setIcon work', () => {
+	const setIcon = testSelect.data( 'fontIconPicker' ).setIcon = jest.fn();
+	testFip.setIcon( '' );
+	expect( setIcon ).toHaveBeenCalledTimes( 1 );
+} );
+
+test( 'API destroyPicker work', () => {
+	const destroy = testSelect.data( 'fontIconPicker' ).destroy = jest.fn();
+	testFip.destroyPicker();
+	expect( destroy ).toHaveBeenCalledTimes( 1 );
+	expect( testSelect.data( 'fontIconPicker' ) ).toBeUndefined();
+} );
+
+test( 'API refreshPicker work', () => {
+	const destroy = testSelect.data( 'fontIconPicker' ).destroy = jest.fn();
+	testFip.refreshPicker();
+	expect( destroy ).toHaveBeenCalledTimes( 1 );
+	expect( testSelect.data( 'fontIconPicker' ) ).toBeDefined();
+} );
+
+test( 'API repositionPicker work', () => {
+	const resetPosition = testSelect.data( 'fontIconPicker' ).resetPosition = jest.fn();
+	testFip.repositionPicker();
+	expect( resetPosition ).toHaveBeenCalledTimes( 1 );
 } );
